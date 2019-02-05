05288826a604663abf12ba8d5a2e9f1290f261d5
Joe Davidson <jdavidso@highpoint.edu>
2019-02-04 21:21:10 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Joseph  Davidson on 2/4/19.
+//  Copyright © 2019 Joseph  Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
