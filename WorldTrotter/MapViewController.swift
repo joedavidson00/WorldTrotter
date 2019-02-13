345031fa3266829b9cf5c2b83599a495fb3ed240
Joe Davidson <jdavidso@highpoint.edu>
2019-02-13 12:42:13 -0500

    finished through page 100

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Joseph  Davidson on 2/4/19.
+//  Copyright © 2019 Joseph  Davidson. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view.")
+    }
+    
+    
+}
+
+
